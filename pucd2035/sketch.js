bcb136c0fb32573715ef6b3c8c6f3f4131c19b9f
matthewjacobson <matthew.g.jacobson@gmail.com>
2019-08-20 14:31:48 -0400

    Update sketch.js

--- pucd2035/sketch.js
+++ pucd2035/sketch.js
@@ -7,45 +7,118 @@ let outline;
 let boundingBox;
 let path;
 
-function drawPathOutline(cmds) {
-  // current pen position
-  let cx = 0;
-  let cy = 0;
-  // start position of current contour
-  let startX = 0;
-  let startY = 0;
-  for (let cmd of cmds) {
-    switch (cmd.type) {
-      case 'M':
-        startX = cmd.x;
-        startY = cmd.y;
-        cx = cmd.x;
-        cy = cmd.y;
-        break;
-      case 'L':
-        line(cx, cy, cmd.x, cmd.y);
-        cx = cmd.x;
-        cy = cmd.y;
-        break;
-      case 'C':
-        bezier(cx, cy, cmd.x1, cmd.y1, cmd.x2, cmd.y2, cmd.x, cmd.y);
-        cx = cmd.x;
-        cy = cmd.y;
-        break;
-      case 'Q':
-        beginShape();
-        vertex(cx, cy);
-        quadraticVertex(cmd.x1, cmd.y1, cmd.x, cmd.y);
-        endShape();
-        cx = cmd.x;
-        cy = cmd.y;
-        break;
-      case 'Z':
-        // to complete path
-        line(cx, cy, startX, startY);
-        break;
+function getBezierPoints(x1, y1, x2, y2, x3, y3, x4, y4) {
+	let output = [];
+	let steps = 20;
+	for (let i = 0; i <= steps; i++) {
+		let t = i / steps;
+		let x = bezierPoint(x1, x2, x3, x4, t);
+		let y = bezierPoint(y1, y2, y3, y4, t);
+		output.push({x: x, y: y});
+	}
+	return output;
+}
+
+function getPathOutline(cmds) {
+
+	// output to store the paths
+	let output = [];
+
+	// current pen position
+	let cx = 0;
+	let cy = 0;
+
+	// start position of current contour
+	let startX = 0;
+	let startY = 0;
+
+	// store the current path
+	let currPath = [];
+
+	for (let cmd of cmds) {
+
+		switch (cmd.type) {
+			case 'M': // move to
+				startX = cmd.x;
+				startY = cmd.y;
+				cx = cmd.x;
+				cy = cmd.y;
+				if (output.length > 0) {
+					output.push(currPath);
+				}
+				currPath = [{x: cx, y: cy}];
+				break;
+			case 'L': // line to
+				line(cx, cy, cmd.x, cmd.y);
+				currPath.push({x: cmd.x, y: cmd.y});
+				cx = cmd.x;
+				cy = cmd.y;
+				break;
+			case 'C': // curve to
+				bezier(cx, cy, cmd.x1, cmd.y1, cmd.x2, cmd.y2, cmd.x, cmd.y);
+				currPath.push(getBezierPoints(cx, cy, cmd.x1, cmd.y1, cmd.x2, cmd.y2, cmd.x, cmd.y));
+				cx = cmd.x;
+				cy = cmd.y;
+				break;
+			case 'Q': // quad to
+				beginShape();
+				vertex(cx, cy);
+				quadraticVertex(cmd.x1, cmd.y1, cmd.x, cmd.y);
+				endShape();
+				currPath.push(getBezierPoints(cx, cy, cmd.x1, cmd.y1, cmd.x1, cmd.y1, cmd.x, cmd.y));
+				cx = cmd.x;
+				cy = cmd.y;
+				break;
+			case 'Z': // close
+				line(cx, cy, startX, startY);
+				currPath.push({x: startX, y: startY});
+				break;
 		}
-  }
+
+	}
+
+	return output;
+
+}
+
+function drawPathOutline(cmds) {
+	// current pen position
+	let cx = 0;
+	let cy = 0;
+	// start position of current contour
+	let startX = 0;
+	let startY = 0;
+	for (let cmd of cmds) {
+		switch (cmd.type) {
+			case 'M': // move to
+				startX = cmd.x;
+				startY = cmd.y;
+				cx = cmd.x;
+				cy = cmd.y;
+				break;
+			case 'L': // line to
+				line(cx, cy, cmd.x, cmd.y);
+				cx = cmd.x;
+				cy = cmd.y;
+				break;
+			case 'C': // curve to
+				bezier(cx, cy, cmd.x1, cmd.y1, cmd.x2, cmd.y2, cmd.x, cmd.y);
+				cx = cmd.x;
+				cy = cmd.y;
+				break;
+			case 'Q': // quad to
+				beginShape();
+				vertex(cx, cy);
+				quadraticVertex(cmd.x1, cmd.y1, cmd.x, cmd.y);
+				endShape();
+				cx = cmd.x;
+				cy = cmd.y;
+				break;
+			case 'Z': // close
+				line(cx, cy, startX, startY);
+				break;
+		}
+	}
 }
 
 function preload() {
@@ -57,6 +130,8 @@ function setup() {
 
 	font = opentype.parse(fontData.bytes.buffer);
 	path = font.getPath(string, 0, 0, 72);
+
+	console.log(getPathOutline(path.commands));
 
 	// var currWidth = 0;
 	// for (var i = 0; i < string.length; i++) {
