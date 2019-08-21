e6703d1b031b9ec428df49e355311014f5e51d7d
matthewjacobson <matthew.g.jacobson@gmail.com>
2019-08-21 15:07:02 -0400

    Update sketch.js

--- pucd2035/sketch.js
+++ pucd2035/sketch.js
@@ -183,23 +183,23 @@ function draw() {
 		let cast = getRayCast(ray);
 		flood.push({angle: angle, x: cast.intersection.x, y: cast.intersection.y});
 	}
-// 	for (let i = 0; i < walls.length; i++) {
-// 		let angle = Math.atan2(walls[i].y1 - mouseY, walls[i].x1 - mouseX);
-// 		let distance = dist(mouseX, mouseY, walls[i].x1, walls[i].y1);
-// 		if (distance < floodSize) {
-// 			let ray = {x: mouseX, y: mouseY, dx: distance * Math.cos(angle), dy: distance * Math.sin(angle)};
-// 			let cast = getRayCast(ray);
-// 			flood.push({angle: angle, x: cast.intersection.x, y: cast.intersection.y});
-// 		}
-// 		let angleLeft = Math.atan2(walls[i].y1 - mouseY, walls[i].x1 - mouseX) - 0.1;
-// 		let rayLeft = {x: mouseX, y: mouseY, dx: floodSize * Math.cos(angleLeft), dy: floodSize * Math.sin(angleLeft)};
-// 		let castLeft = getRayCast(rayLeft);
-// 		flood.push({angle: angleLeft, x: castLeft.intersection.x, y: castLeft.intersection.y});
-// 		let angleRight = Math.atan2(walls[i].y1 - mouseY, walls[i].x1 - mouseX) + 0.1;
-// 		let rayRight = {x: mouseX, y: mouseY, dx: floodSize * Math.cos(angleRight), dy: floodSize * Math.sin(angleRight)};
-// 		let castRight = getRayCast(rayRight);
-// 		flood.push({angle: angleRight, x: castRight.intersection.x, y: castRight.intersection.y});
-// 	}
+	for (let i = 0; i < walls.length; i++) {
+		let angle = Math.atan2(walls[i].y1 - mouseY, walls[i].x1 - mouseX);
+		let distance = dist(mouseX, mouseY, walls[i].x1, walls[i].y1);
+		if (distance < floodSize) {
+			let ray = {x: mouseX, y: mouseY, dx: distance * Math.cos(angle), dy: distance * Math.sin(angle)};
+			let cast = getRayCast(ray);
+			flood.push({angle: angle, x: cast.intersection.x, y: cast.intersection.y});
+		}
+		let angleLeft = Math.atan2(walls[i].y1 - mouseY, walls[i].x1 - mouseX) - 0.1;
+		let rayLeft = {x: mouseX, y: mouseY, dx: floodSize * Math.cos(angleLeft), dy: floodSize * Math.sin(angleLeft)};
+		let castLeft = getRayCast(rayLeft);
+		flood.push({angle: angleLeft, x: castLeft.intersection.x, y: castLeft.intersection.y});
+		let angleRight = Math.atan2(walls[i].y1 - mouseY, walls[i].x1 - mouseX) + 0.1;
+		let rayRight = {x: mouseX, y: mouseY, dx: floodSize * Math.cos(angleRight), dy: floodSize * Math.sin(angleRight)};
+		let castRight = getRayCast(rayRight);
+		flood.push({angle: angleRight, x: castRight.intersection.x, y: castRight.intersection.y});
+	}
 	flood.sort((a, b) => a.angle - b.angle);
 
 	if (frameCount == 1) {
